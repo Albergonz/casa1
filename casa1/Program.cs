8d1e06156ba3f0805d3baf9a50419497ae922f6e
Acer <Acer@LAPTOP-9SISV8A7>
2024-01-23 16:49:44 +0100

    Aggiungere i file di progetto.

---
+++ casa1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casa1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio r = new Radio();
+            r.Aumenta();
+            r.Diminuisci();
+        }
+    }
+}
---
+++ casa1/Radio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casa1
+{
+    internal class Radio
+    {
+        public float Volume { get; set; }
+        public float Aumenta()
+        {
+            Volume = Volume + ((10 * Volume) / 100);
+            return Volume;
+        }
+
+        public float Diminuisci()
+        {
+            Volume = Volume - ((10 * Volume) / 100);
+            return Volume;
+        }
+    }
+}
